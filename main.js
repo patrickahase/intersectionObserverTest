3d658761e78733748a4a2146857af150a3fcb2ab
hase <62376587+patrickahase@users.noreply.github.com>
2026-05-10 12:14:12 +1000

    init
    set up really basic test based on scroll and viewport intersection with a p element

---
+++ main.js
@@ -0,0 +1,33 @@
+const mainEl = document.getElementById("mainSection");
+const checkThis = document.getElementById("checkThis");
+
+let currentHue = 0;
+
+const iOOptions = {
+    root: null,
+    rootMargin: "0px",
+    scrollMargin: "0px",
+    threshold: 1.0,
+}
+
+function updateBGColour(){
+
+    currentHue += 40;
+    mainEl.style.backgroundColor = `hsl(${currentHue}, 10%, 50%)`;
+}
+
+const intersectionCallback = (entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            let elem = entry.target;
+
+            if (entry.intersectionRatio >= 0.75) {
+                updateBGColour();
+            }
+        }
+    });
+};
+
+const observer = new IntersectionObserver(intersectionCallback, iOOptions);
+
+observer.observe(checkThis);
